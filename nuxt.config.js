5d377715ac4dd23dbaf35afdcedc96fcb7e5f190
nguyen trung kien <nguyentrungkien@MacBook-Air-cua-nguyen.local>
2020-03-27 14:33:33 +0700

    testing

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   server: {
     port:8080, // default: 3000
-    host: 'localhost' // default: localhost
+    host: '103.226.249.122' // default: localhost
   },
   sms:{
     // Token send sms 
